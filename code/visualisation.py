81112093a6ccb14f5e9a470b97a43569d28a6ce3
darkhyper24 <s-mohammed.elmaadawy@zewailcity.edu.eg>
2024-12-28 01:16:41 +0200

    updated code from amr

--- code/visualisation.py
+++ code/visualisation.py
@@ -1,21 +1,28 @@
-import plotly.graph_objects as go
+import matplotlib.pyplot as plt
 import numpy as np
+
 
 def visualize_grid_path(grid, path, start, goal, title):
     grid_size = len(grid)
-    fig = go.Figure()
+    fig, nx = plt.subplots(figsize=(6, 6))
+    
+    nx.set_xlim(0, grid_size)
+    nx.set_ylim(0, grid_size)
+    nx.set_xticks(np.arange(0, grid_size + 1, 1))
+    nx.set_yticks(np.arange(0, grid_size + 1, 1))
+    nx.set_aspect('equal')
+
+    nx.add_patch(plt.Rectangle((start[1], grid_size - start[0] - 1), 1, 1, color='blue'))
+    nx.add_patch(plt.Rectangle((goal[1], grid_size - goal[0] - 1), 1, 1, color='green'))
 
     for i in range(grid_size):
         for j in range(grid_size):
             if grid[i][j] == "X":
-                fig.add_shape(type="rect", x0=j, y0=grid_size - i - 1, x1=j+1, y1=grid_size - i, fillcolor="red", line=dict(color="red"))
-
-    fig.add_shape(type="rect", x0=start[1], y0=grid_size - start[0] - 1, x1=start[1]+1, y1=grid_size - start[0], fillcolor="blue", line=dict(color="blue"))
-    fig.add_shape(type="rect", x0=goal[1], y0=grid_size - goal[0] - 1, x1=goal[1]+1, y1=grid_size - goal[0], fillcolor="green", line=dict(color="green"))
-
+                nx.add_patch(plt.Rectangle((j, grid_size - i - 1), 1, 1, color='red'))
+    
     for (x, y) in path:
         if (x, y) != start and (x, y) != goal:
-            fig.add_shape(type="rect", x0=y, y0=grid_size - x - 1, x1=y+1, y1=grid_size - x, fillcolor="yellow", line=dict(color="yellow"))
+            nx.add_patch(plt.Rectangle((y, grid_size - x - 1), 1, 1, color='yellow'))
 
-    fig.update_layout(title=title, xaxis=dict(tickmode='array', tickvals=list(range(grid_size+1))), yaxis=dict(tickmode='array', tickvals=list(range(grid_size+1))), showlegend=False)
-    fig.show()
+    plt.title(title)
+    plt.show()
